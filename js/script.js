9694ac412d38bf7ee88ca90e43cfaf0c2c230fd4
renadnajj <renadnajj24@gmail.com>
2022-09-20 17:27:57 +0300

    paralex

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// let m1=document.querySelector("#m1");
+// let m1=document.querySelector("#t2");
+// let m1=document.querySelector("#m2");
+// let m1=document.querySelector("#t1");
+// let m1=document.querySelector("#m");
+// let m1=document.querySelector("#plants");
+// gsap.from(m1,{
+//     scrollT
+// })
